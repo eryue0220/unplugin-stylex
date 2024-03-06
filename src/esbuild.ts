2a332a971c0f0a42a78e5b833a26af4bce8e7678
cinchen <eryue0220@gmail.com>
2024-03-06 16:48:33 +0800

    fix: jsr publish bug (#25)
    * fix: support jsr
    
    * fix: support jsr

--- src/esbuild.ts
+++ src/esbuild.ts
@@ -1,6 +1,8 @@
 import { createEsbuildPlugin } from 'unplugin'
+import type { EsbuildPlugin } from 'unplugin'
 import { unpluginFactory } from './index'
+import type { UnpluginStylexInstance } from './types'
 
-const esbuildPlugin = createEsbuildPlugin(unpluginFactory)
+const esbuildPlugin: UnpluginStylexInstance<EsbuildPlugin | EsbuildPlugin[]> = createEsbuildPlugin(unpluginFactory)
 
 export default esbuildPlugin
--- src/rspack.ts
+++ src/rspack.ts
@@ -1,6 +1,10 @@
 import { createRspackPlugin } from 'unplugin'
+import type { RspackPluginInstance } from 'unplugin'
 import { unpluginFactory } from './index'
+import type { UnpluginStylexInstance } from './types'
 
-const rspackPlugin = createRspackPlugin(unpluginFactory)
+type RspackPluginType = UnpluginStylexInstance<RspackPluginInstance | RspackPluginInstance[]>
+
+const rspackPlugin: RspackPluginType = createRspackPlugin(unpluginFactory)
 
 export default rspackPlugin
