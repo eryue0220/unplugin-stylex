69aef20a2f46e659e992f25440f6ba0eee0e7c06
cinchen <eryue0220@gmail.com>
2024-03-06 17:25:15 +0800

    feat: upgrade jsr info (#26)

--- src/rspack.ts
+++ src/rspack.ts
@@ -1,3 +1,9 @@
+/**
+ * This entry file is for Rspac plugin.
+ *
+ * @module
+ */
+
 import { createRspackPlugin } from 'unplugin'
 import type { RspackPluginInstance } from 'unplugin'
 import { unpluginFactory } from './index'
@@ -7,4 +13,17 @@ type RspackPluginType = UnpluginStylexInstance<RspackPluginInstance | RspackPlug
 
 const rspackPlugin: RspackPluginType = createRspackPlugin(unpluginFactory)
 
+/**
+ * Rsapck plugin
+ *
+ * @example
+ *
+ * import stylexPlugin from 'unplugin-stylex/rspack'
+ *
+ * module.exports = {
+ *   plugins: [
+ *     stylexPlugin(),
+ *   ],
+ * }
+ */
 export default rspackPlugin
--- src/vite.ts
+++ src/vite.ts
@@ -1,3 +1,9 @@
+/**
+ * This entry file is for Vite plugin.
+ *
+ * @module
+ */
+
 import { createVitePlugin } from 'unplugin'
 import type { VitePlugin } from 'unplugin'
 import { unpluginFactory } from './index'
@@ -5,4 +11,18 @@ import type { UnpluginStylexInstance } from './types'
 
 const vitePlugin: UnpluginStylexInstance<VitePlugin | VitePlugin[]> = createVitePlugin(unpluginFactory)
 
+/**
+ * Vite example
+ *
+ * @example
+ *
+ * import { defineConfig } from 'vite'
+ * import stylexPlugin from 'unplugin-stylex/vite'
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     stylexPlugin(),
+ *   ],
+ * })
+ */
 export default vitePlugin
