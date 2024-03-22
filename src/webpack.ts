2e8f7a3e952f6fde27a64c0f9e0908b100c1044c
eryue0220 <eryue0220@gmail.com>
2024-03-22 12:11:09 +0800

    feat: support webpack

--- src/webpack.ts
+++ src/webpack.ts
@@ -4,14 +4,24 @@
  * @module
  */
 
-import { isDevelopment } from './core/constants'
-import type { UnpluginStylexOptions } from './types'
+import { createWebpackPlugin } from 'unplugin'
+import type { WebpackPluginInstance } from 'unplugin'
+import { unpluginFactory } from './index'
+import type { UnpluginStylexInstance } from './types'
+
+const webpackPlugin: UnpluginStylexInstance<WebpackPluginInstance> = createWebpackPlugin(unpluginFactory)
 
 /**
- * Note: Current, please use @stylexjs/webpack-plugin
+ * Webpack plugin
+ *
+ * @example
+ *
+ * import stylexPlugin from 'unplugin-stylex/webpack'
+ *
+ * module.exports = {
+ *   plugins: [
+ *     stylexPlugin(),
+ *   ],
+ * }
  */
-export default (options?: UnpluginStylexOptions) => {
-  if (isDevelopment || options?.dev) {
-    throw new Error('If you want to use this plugin through webpack, please use "@stylexjs/webpack-plugin" instead')
-  }
-}
+export default webpackPlugin
