5431923ecfc3eab00d614eb26a8f4cb996882275
eryue0220 <eryue0220@gmail.com>
2024-03-25 18:29:25 +0800

    feat: upgrade

--- examples/farm-example/farm.config.js
+++ examples/farm-example/farm.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from '@farmfe/core'
 import stylexFarmPlugin from 'unplugin-stylex/farm'
 
 export default defineConfig({
+  compilation: {
+    input: {
+      index: './index.html'
+    },
+    output: {
+      path: 'dist',
+      publicPath: '/',
+      targetEnv: 'browser'
+    },
+  },
   plugins: [
     '@farmfe/plugin-react',
-    stylexFarmPlugin({
-      dev: true,
-      stylex: {
-        useCSSLayers: true,
-        genConditionalClasses: true,
-        treeshakeCompensation: true,
-      },
-    }),
+    stylexFarmPlugin({ dev: true }),
   ],
 });
