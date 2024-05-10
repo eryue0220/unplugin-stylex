80a68a098cd0913ddea6368d6c23c3a1a6f3004d
eryue0220 <eryue0220@gmail.com>
2024-05-10 18:59:09 +0800

    fix: type bug

--- src/types.ts
+++ src/types.ts
@@ -6,16 +6,18 @@ export type BabelConfig = {
 
 export type StylexOptions = {
   filename?: string
+  aliases?: string[]
   stylexImports?: string[]
   classNamePrefix?: string
-  useCSSLayers?:  boolean
   unstable_moduleResolution?: {
     type: 'commonjs' | 'haste'
     rootDir: string
   }
   babelConfig?: BabelConfig
-  runtimeInjection: boolean
-  aliases?: string[]
+  useCSSLayers?:  boolean
+  genConditionalClasses?: boolean
+  treeshakeCompensation?: boolean
+  runtimeInjection?: boolean
 }
 
 export type UnpluginStylexOptions = {
