2f2ee6b38d1940ac96c6fa9d4349970aa19871fb
eryue0220 <eryue0220@gmail.com>
2024-07-10 16:52:29 +0800

    feat: support rolldown

---
+++ src/rolldown.ts
@@ -0,0 +1,29 @@
+/**
+ * This entry file is for rolldown plugin.
+ *
+ * @module
+ */
+
+import { createRolldownPlugin } from 'unplugin'
+import { unpluginFactory } from './index'
+
+const rolldownPlugin = createRolldownPlugin(unpluginFactory)
+
+
+/**
+ * @experimental
+ *
+ * Rolldown plugin
+ *
+ * @example
+ *
+ * import { build } from 'esbuild'
+ * import stylexPlugin from 'unplugin-stylex/rolldown'
+ *
+ * export default {
+ *   plugins: [
+ *     stylexPlugin(),
+ *   ],
+ * }
+ */
+export default rolldownPlugin
