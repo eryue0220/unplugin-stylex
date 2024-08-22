2a84bf43396f3410abfb81c6b921cbc22ce4faf1
eryue0220 <eryue0220@gmail.com>
2024-08-22 23:15:20 +0800

    feat: update style

---
+++ examples/remix-example/app/root.tsx
@@ -0,0 +1,50 @@
+import {
+  Form,
+  Links,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div
+                aria-hidden
+                hidden={true}
+                id="search-spinner"
+              />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
