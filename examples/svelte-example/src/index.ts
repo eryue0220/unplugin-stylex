ccfb5180c8c1a6a4fc3baa0766f235efd9ac4eae
eryue0220 <eryue0220@gmail.com>
2025-11-07 17:36:45 +0800

    fix: ci

--- examples/svelte-example/src/index.ts
+++ examples/svelte-example/src/index.ts
@@ -1,6 +1,6 @@
-import { mount } from 'svelte';
-import App from "./app.svelte";
+import { mount } from 'svelte'
+import App from './app.svelte'
 
-const app = mount(App, { target: document.getElementById('root') as HTMLElement });
+const app = mount(App, { target: document.getElementById('root') as HTMLElement })
 
-export default app;
+export default app
--- examples/svelte-example/vite.config.js
+++ examples/svelte-example/vite.config.js
@@ -1,29 +1,29 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
 import stylexVitePlugin from 'unplugin-stylex/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	build: {
-		outDir: 'dist',
-	},
-	optimizeDeps: {
-		exclude: ['@stylexjs/open-props'],
-	},
-	ssr: {
-		noExternal: ['@stylexjs/open-props', '@stylexjs/stylex'],
-	},
-	plugins: [
-		svelte(),
-		{
-			...stylexVitePlugin({
-				dev: true,
-				stylex: {
-					useCSSLayers: true,
-					genConditionalClasses: true,
-					treeshakeCompensation: true,
-				},
-			}),
-			enforce: 'pre',
-		},
-	],
+  build: {
+    outDir: 'dist',
+  },
+  optimizeDeps: {
+    exclude: ['@stylexjs/open-props'],
+  },
+  ssr: {
+    noExternal: ['@stylexjs/open-props', '@stylexjs/stylex'],
+  },
+  plugins: [
+    svelte(),
+    {
+      ...stylexVitePlugin({
+        dev: true,
+        stylex: {
+          useCSSLayers: true,
+          genConditionalClasses: true,
+          treeshakeCompensation: true,
+        },
+      }),
+      enforce: 'pre',
+    },
+  ],
 })
