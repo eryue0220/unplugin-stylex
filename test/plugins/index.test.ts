b9e8a0a95aec3226537462e569de28533df6d526
eryue0220 <eryue0220@gmail.com>
2026-01-27 18:27:34 +0800

    ci

--- test/plugins/index.test.ts
+++ test/plugins/index.test.ts
@@ -7,14 +7,11 @@ describe('main unplugin', () => {
   })
 
   it('should be callable', () => {
-    // unplugin is created by createUnplugin which returns an object with a callable property
     expect(unplugin).toBeDefined()
-    // Check if it has the expected structure
     expect(typeof unplugin).toBe('object')
   })
 
   it('should return plugin when called', () => {
-    // unplugin from createUnplugin is an object that can be called
     const plugin = typeof unplugin === 'function' ? unplugin() : unplugin
     expect(plugin).toBeDefined()
   })
